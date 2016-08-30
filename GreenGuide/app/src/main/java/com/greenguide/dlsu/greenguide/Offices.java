892f93de4017f3eeea978835a7af3ce19a12a8f6
Jerrick Estioko <Jerrick Estioko>
2016-08-30 23:53:00 +0800

    Lasallian Mission Office (correction)

--- GreenGuide/app/src/main/java/com/greenguide/dlsu/greenguide/Offices.java
+++ GreenGuide/app/src/main/java/com/greenguide/dlsu/greenguide/Offices.java
@@ -31,7 +31,7 @@ public class Offices extends android.app.Fragment{
         List list = new ArrayList();
         list.add("Administrative Offices");
         list.add("Student Affairs Offices");
-        //list.add("Lasallian Missions Offices"); @TODO
+        //list.add("Lasallian Mission Office"); @TODO
         adapter = new CommonAdapter(list);
         recycler = (RecyclerView) v.findViewById(R.id.officeslist);
         recycler.setHasFixedSize(true);
