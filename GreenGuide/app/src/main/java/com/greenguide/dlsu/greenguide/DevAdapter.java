27073660e5b213c00e3639e2478bb99fe893641f
Jerrick Estioko <Jerrick Estioko>
2016-09-04 15:59:33 +0800

    DLSU LPEP Green Guide v1.3 Release Version

--- GreenGuide/app/src/main/java/com/greenguide/dlsu/greenguide/DevAdapter.java
+++ GreenGuide/app/src/main/java/com/greenguide/dlsu/greenguide/DevAdapter.java
@@ -7,8 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.uncopt.android.widget.text.justify.JustifiedTextView;
-
 import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
